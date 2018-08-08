85d2c7192b9643b5bed2e6d9f5722d49f8e3c6fb
Eminarcissus <eminarcissus@gmail.com>
2018-08-08 16:12:14 +0800

    make constructors any? compatible

--- Sources/Reflection/Construct.swift
+++ Sources/Reflection/Construct.swift
@@ -1,9 +1,9 @@
 /// Create a struct with a constructor method. Return a value of `property.type` for each property.
-public func construct<T>(_ type: T.Type = T.self, constructor: (Property.Description) throws -> Any) throws -> T {
+public func construct<T>(_ type: T.Type = T.self, constructor: (Property.Description) throws -> Any?) throws -> T {
     return try constructGenericType(constructor: constructor)
 }
 
-func constructGenericType<T>(_ type: T.Type = T.self, constructor: (Property.Description) throws -> Any) throws -> T {
+func constructGenericType<T>(_ type: T.Type = T.self, constructor: (Property.Description) throws -> Any?) throws -> T {
     if Metadata(type: T.self)?.kind == .struct {
         return try constructValueType(constructor)
     } else {
@@ -12,20 +12,20 @@ func constructGenericType<T>(_ type: T.Type = T.self, constructor: (Property.Des
 }
 
 /// Create a struct with a constructor method. Return a value of `property.type` for each property.
-public func construct(_ type: Any.Type, constructor: (Property.Description) throws -> Any) throws -> Any {
+public func construct(_ type: Any.Type, constructor: (Property.Description) throws -> Any?) throws -> Any? {
     return try extensions(of: type).construct(constructor: constructor)
 }
 
-private func constructValueType<T>(_ constructor: (Property.Description) throws -> Any) throws -> T {
+private func constructValueType<T>(_ constructor: (Property.Description) throws -> Any?) throws -> T {
     guard Metadata(type: T.self)?.kind == .struct else { throw ReflectionError.notStruct(type: T.self) }
     let pointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
     defer { pointer.deallocate() }
-    var values: [Any] = []
+    var values: [Any?] = []
     try constructType(storage: UnsafeMutableRawPointer(pointer), values: &values, properties: properties(T.self), constructor: constructor)
     return pointer.move()
 }
 
-private func constructType(storage: UnsafeMutableRawPointer, values: inout [Any], properties: [Property.Description], constructor: (Property.Description) throws -> Any) throws {
+private func constructType(storage: UnsafeMutableRawPointer, values: inout [Any?], properties: [Property.Description], constructor: (Property.Description) throws -> Any?) throws {
     var errors = [Error]()
     for property in properties {
         do {
@@ -42,16 +42,16 @@ private func constructType(storage: UnsafeMutableRawPointer, values: inout [Any]
 }
 
 /// Create a struct from a dictionary.
-public func construct<T>(_ type: T.Type = T.self, dictionary: [String: Any]) throws -> T {
+public func construct<T>(_ type: T.Type = T.self, dictionary: [String: Any?]) throws -> T {
     return try constructGenericType(constructor: constructorForDictionary(dictionary))
 }
 
 /// Create a struct from a dictionary.
-public func construct(_ type: Any.Type, dictionary: [String: Any]) throws -> Any {
+public func construct(_ type: Any.Type, dictionary: [String: Any?]) throws -> Any? {
     return try construct(type, constructor: constructorForDictionary(dictionary))
 }
 
-private func constructorForDictionary(_ dictionary: [String: Any]) -> (Property.Description) throws -> Any {
+private func constructorForDictionary(_ dictionary: [String: Any?]) -> (Property.Description) throws -> Any? {
     return { property in
         if let value = dictionary[property.key] {
             return value
